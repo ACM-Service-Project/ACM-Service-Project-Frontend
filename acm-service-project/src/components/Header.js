dca7fc118523d7781c1940fc78dca27d2e3913fe
Alexander Berryhill <amsb99@gmail.com>
2023-02-28 21:08:59 -0700

    Fix Login Screen

--- acm-service-project/src/components/Header.js
+++ acm-service-project/src/components/Header.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import '../styles/Header.css';
 import profilePic from '../assets/defaultProfile.png';
+import { useLocation } from 'react-router-dom'
 
-function Header(){
+function Header(props){
+    let location = useLocation();
+    console.log(location)
+    if (location.pathname==='/'){
+        return null;
+    }
     return(
         <div className='header'>
             <img className='profile-img' src={profilePic} alt='Profile'></img>
