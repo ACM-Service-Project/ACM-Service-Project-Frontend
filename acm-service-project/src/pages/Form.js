757c03644634febc6a91986865fc3cc86d9f1c03
alexis <91147834+alexis-rudy@users.noreply.github.com>
2023-03-06 13:46:30 -0700

    added html

--- acm-service-project/src/pages/Form.js
+++ acm-service-project/src/pages/Form.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 function Form(){
     return(
-        <div>
-            
+        <div className='mainContainer'>
+            <fieldset>
+                <h3>ADD NEW PERSON</h3>
+                <form>
+                    <label>First Name</label><input type='text' name='fName'></input>
+                    <label>Last Name</label><input type='text' name='lName'></input>
+                    <label>Address</label><input type='' name='address'></input>
+                    <label>Phone Number</label><input type='number' name='pNumber'></input>
+                    <label>Email</label><input type='email' name='email'></input>
+                </form>
+                <button id='addPerson' type='submit'>ADD</button>
+            </fieldset>
         </div>
     );
 }
