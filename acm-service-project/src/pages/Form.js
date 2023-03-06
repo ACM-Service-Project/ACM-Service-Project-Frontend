4edd0c8c0a01701b60516e9be263497f610f4056
alexis <91147834+alexis-rudy@users.noreply.github.com>
2023-03-06 13:49:23 -0700

    fixed input type for number

--- acm-service-project/src/pages/Form.js
+++ acm-service-project/src/pages/Form.js
@@ -8,8 +8,8 @@ function Form(){
                 <form>
                     <label>First Name</label><input type='text' name='fName'></input>
                     <label>Last Name</label><input type='text' name='lName'></input>
-                    <label>Address</label><input type='' name='address'></input>
-                    <label>Phone Number</label><input type='number' name='pNumber'></input>
+                    <label>Address</label><input type='text' name='address'></input>
+                    <label>Phone Number</label><input type='tel' name='pNumber'></input>
                     <label>Email</label><input type='email' name='email'></input>
                 </form>
                 <button id='addPerson' type='submit'>ADD</button>
