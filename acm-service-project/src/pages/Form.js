bf071ca0ab5485b47e768931402068d647e5bd3b
alexis <91147834+alexis-rudy@users.noreply.github.com>
2023-03-07 17:29:25 -0700

    added css

--- acm-service-project/src/pages/Form.js
+++ acm-service-project/src/pages/Form.js
@@ -7,11 +7,11 @@ function Form(){
             <fieldset>
                 <h3>ADD NEW PERSON</h3>
                 <form>
-                    <label>First Name</label><input type='text' name='fName'></input>
-                    <label>Last Name</label><input type='text' name='lName'></input>
-                    <label>Address</label><input type='text' name='address'></input>
-                    <label>Phone Number</label><input type='tel' name='pNumber'></input>
-                    <label>Email</label><input type='email' name='email'></input>
+                    <label class='fname'>First Name</label><input type='text' name='fName' class='fname'></input>
+                    <label class='lname'>Last Name</label><input type='text' name='lName' class='lname'></input>
+                    <label class='address'>Address</label><input type='text' name='address' class='address'></input>
+                    <label class='pnumber'>Phone Number</label><input type='tel' name='pNumber' class='pnumber'></input>
+                    <label class='email'>Email</label><input type='email' name='email' class='email'></input>
                 </form>
                 <button id='addPerson' type='submit'>ADD</button>
             </fieldset>
