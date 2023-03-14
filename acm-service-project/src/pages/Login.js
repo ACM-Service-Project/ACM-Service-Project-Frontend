4de25cf1369311a07530ec825f750cf9c7a3bc22
Alexander Berryhill <amsb99@gmail.com>
2023-03-14 12:04:23 -0600

    Fixed merge CSS

--- acm-service-project/src/pages/Login.js
+++ acm-service-project/src/pages/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import '../styles/login.css';
+import '../styles/Login.css';
 import LoginLogo from '../assets/loginLogo.png';
 
 function Login(){
