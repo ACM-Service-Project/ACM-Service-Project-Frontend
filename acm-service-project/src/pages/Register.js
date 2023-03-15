b7d95ed6fba6422481434a0e2485673e26372360
Jacob Brooks <brookscollegejake@gmail.com>
2023-03-14 19:28:19 -0600

    added input forms to register page

--- acm-service-project/src/pages/Register.js
+++ acm-service-project/src/pages/Register.js
@@ -12,14 +12,27 @@ function Register(){
                     <img className='login_logo' src={LoginLogo} alt='Family Crisis Center Logo'/>
                     <p className='title'>FOOD BANK</p>
                     <form>
-                        <label for='username'>Username</label>
-                        <input type="text" className='input' id='username' placeholder='Username' name='username'/>
+                        <label for='firstname'>First Name</label>
+                        <input type="text" className='input' id='firstname' placeholder='First Name' name='firstname'/>
+
+                        <label for='lastname'>Last Name</label>
+                        <input type="text" className='input' id='lastname' placeholder='Last Name' name='lastname'/>
+
+                        <label for='phonenumber'>Phone Number</label>
+                        <input type="text" className='input' id='phonenumber' placeholder='phonenumber' name='phonenumber'/>
+
                         <label for='email'>Email</label>
                         <input type="email" className='input' id='email' placeholder='Email' name='email'/>
+
+                        <label for='username'>Usename</label>
+                        <input type="text" className='input' id='username' placeholder='Username' name='username'/>
+
                         <label for='password'>Password</label>
                         <input type="password" className='input' id='password' placeholder='Password' name='password'/>
+
                         <label for='confirm_password'>Confirm Password</label>
                         <input type="password" className='input' id='confirm_password' placeholder='Confirm Password' name='confirm_password'/>
+
                         <button type="submit" id='submit'>Create Account</button>
                     </form>
                     <Link to = '/' className='login_link'>
@@ -28,7 +41,7 @@ function Register(){
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Register;
