7c119a907229d06585f6d9f21678a4da102cfeff
spencergamero23 <122706033+spencergamero23@users.noreply.github.com>
2023-03-21 20:13:21 -0600

    Merge branch 'main' of https://github.com/ACM-Service-Project/ACM-Service-Project-Frontend

--- acm-service-project/src/components/Sidebar.js
+++ acm-service-project/src/components/Sidebar.js
@@ -9,6 +9,7 @@ import profile from '../assets/profile.png';
 import settings from '../assets/settings.png';
 import Export from '../assets/export.png'; 
 import { useLocation } from 'react-router-dom'
+import check from '../assets/check.png';
 
 function Sidebar(){
     let location = useLocation();
@@ -18,22 +19,29 @@ function Sidebar(){
     }
     return (
         <div id='sidebar'>
-            <img src={logo} alt='Logo' className='sidebar-link'></img>
-            <img src={familyCrisisCenter} alt='Family Crisis Center' className='fcc-title'></img>
+            <div  class = 'logo'>
+                <img src={logo} alt='Logo' className='sidebar-link'></img>
+                <img src={familyCrisisCenter} alt='Family Crisis Center' className='fcc-title'></img>
+            </div>
             <Link to='/dashboard' className='link'>
-                <img src={home} alt='Home' className='sidebar-link'></img>
+                <img src={home} alt='Home' className='home_img'></img> 
+                <a className='sidebar_a' >Home</a>
             </Link>
             <Link to='/search' className='link'>
-                <img src={search} alt='Search' className='sidebar-link'></img>
+                <img src={check} alt='check' className='check_img'></img>
+                <a className='sidebar_a' >Check-in</a>
             </Link>
             <Link to='/form' className='link'>
-                <img src={profile} alt='Profile' className='sidebar-link'></img>
+                <img src={profile} alt='Profile' className='profile_img'></img>
+                <a className='sidebar_a' >Directory</a>
             </Link>
             <Link className='link'>
-                <img src={settings} alt='Settings' className='sidebar-link'></img>
+                <img src={settings} alt='Settings' className='settings_img'></img>
+                <a className='sidebar_a' >Settings</a>  
             </Link>
             <Link className='link'>
-                <img src={Export} alt='Export' className='sidebar-link'></img>
+                <img src={Export} alt='Export' className='export_img'></img>
+                <a className='sidebar_a'>Logout</a>
             </Link>
         </div>
     );
